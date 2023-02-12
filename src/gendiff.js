334f4ea242df702f38536377c9cd0d2b4d978fd3
Holerik <a_zobnin@mail.ru>
2023-02-12 18:30:17 +0300

    Аргументы и опции

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,10 +3,17 @@
 
 import { program } from 'commander';
 
+const genDiff = (files) => {
+  return '';
+}
+
 program
   .description('Compares two configuration files and shows a difference.\n')
   .option('-h, --help', 'display help for command')
-  .version('0.0.1', '-v, --version', 'output the version number');
+  .version('0.0.1', '-v, --version', 'output the version number')
+  .option('-f, --format <type>  output format')
+  .arguments('<filepath1> <filepath2>', 'files to compare')
+  .action(genDiff);
 
 program.parse(process.argv);
 
